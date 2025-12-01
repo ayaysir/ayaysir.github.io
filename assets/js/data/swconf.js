0a54fa50be768f14260291168544c00262c8a2e8
ayaysir <ayaysir@users.noreply.github.com>
2025-12-01 10:49:22 +0000

    deploy: 5d3cfe415c3ef6a74d33f3f459b99eb184475975

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1764496991',resources: [
+    cacheName: 'chirpy-1764586146',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
